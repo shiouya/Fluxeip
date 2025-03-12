c16376b56bd1e5f49d10fe6b8e7dba46ce7d0a78
shiouya <s54070639@example.com>
2025-03-12 11:45:09 +0800

    加入角色

--- src/main/java/com/example/fluxeip/model/Employee.java
+++ src/main/java/com/example/fluxeip/model/Employee.java
@@ -1,6 +1,8 @@
 package com.example.fluxeip.model;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -10,6 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -54,7 +57,9 @@ public class Employee {
 	@JsonIgnore
 	@OneToOne(mappedBy = "employee")
 	private EmployeeDetail employeeDetail;
-//
+
+	@ManyToMany(mappedBy = "employee")
+	private List<Roles> roles = new LinkedList<Roles>();
 //	@OneToMany(mappedBy = "supervisor", cascade = CascadeType.ALL)
 //	private List<WorkProgess> workprogess = new LinkedList<WorkProgess>();
 //
@@ -64,8 +69,6 @@ public class Employee {
 //	@OneToMany(mappedBy = "reveiew", cascade = CascadeType.ALL)
 //	private List<Taskassign> reveiew = new LinkedList<Taskassign>();
 //
-//	@ManyToMany(mappedBy = "employee")
-//	private List<Roles> roles = new LinkedList<Roles>();
 
 	public Employee() {
 	}
@@ -73,14 +76,5 @@ public class Employee {
     public Employee(Integer employeeId) {
         this.employeeId = employeeId;
     }
-
-
-//	public List<WorkProgess> getWorkprogess() {
-//		return workprogess;
-//	}
-//
-//	public void setWorkprogess(List<WorkProgess> workprogess) {
-//		this.workprogess = workprogess;
-//	}
 
 }
