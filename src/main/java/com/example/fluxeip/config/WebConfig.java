859d1ac9d4be333879c0f80be21227ffa54f863f
HUANGHAIHSIANG <a4567327@gmail.com>
2025-03-18 12:48:36 +0800

    測試

--- src/main/java/com/example/fluxeip/config/WebConfig.java
+++ src/main/java/com/example/fluxeip/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.fluxeip.config;
 import java.nio.file.Paths;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/uploads/images/**")
                 .addResourceLocations("file:" + uploadDir + "/");
     }
+    
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173")  // 允許的前端地址
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 允許的 HTTP 方法
+//                .allowedHeaders("*")  // 允許所有標頭
+//                .allowCredentials(true);  // 允許攜帶憑證（如 Cookie）
+//    }
 }
 
---
+++ src/main/java/com/example/fluxeip/repository/PersonalCalendarRepository.java
@@ -0,0 +1,9 @@
+package com.example.fluxeip.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.fluxeip.model.PersonalCalendar;
+
+public interface PersonalCalendarRepository extends JpaRepository<PersonalCalendar, Integer>{
+
+}
