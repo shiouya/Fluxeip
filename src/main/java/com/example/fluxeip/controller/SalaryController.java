4da1ac783b78bb83660cc2dadb33fef6afacc7c4
money <e020435@gmail.com>
2025-03-20 15:30:58 +0800

    putmapping修正

--- src/main/java/com/example/fluxeip/controller/SalaryController.java
+++ src/main/java/com/example/fluxeip/controller/SalaryController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -68,7 +69,7 @@ public class SalaryController {
 		}
 	}
 	
-	@PostMapping("/{id}")
+	@PutMapping("/{id}")
 	public ResponseEntity<?> updateSalarySetting(@PathVariable("id") Integer empId,@RequestBody SalaryDefaultSetting salaryDefaultSetting){
 		
 		try {
