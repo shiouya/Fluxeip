3915d3f86410508d0c555f69975e0d68827e1490
Mqsun <shawn.sun0328@gmail.com>
2025-03-26 02:12:56 +0800

    通知邏輯

--- src/main/java/com/example/fluxeip/controller/NotifyController.java
+++ src/main/java/com/example/fluxeip/controller/NotifyController.java
@@ -1,5 +1,60 @@
 package com.example.fluxeip.controller;
 
+import com.example.fluxeip.dto.NotifyRequest;
+import com.example.fluxeip.dto.NotifyResponse;
+import com.example.fluxeip.service.NotifyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/notify")
 public class NotifyController {
+
+	@Autowired
+	private NotifyService notifyService;
+
+	// 查詢員工的通知清單
+	@GetMapping("/{employeeId}")
+	public ResponseEntity<List<NotifyResponse>> getNotificationsByEmployeeId(@PathVariable Integer employeeId) {
+		List<NotifyResponse> responseList = notifyService.findAllByEmployeeId(employeeId);
+		return ResponseEntity.ok(responseList);
+	}
+
+	// 通知已讀
+	@PutMapping("/read/{id}")
+	public ResponseEntity<?> markAsRead(@PathVariable Integer id) {
+
+		Optional<NotifyResponse> result = notifyService.markAsRead(id);
+
+		if (result.isPresent()) {
+			return ResponseEntity.ok(result.get());
+		} else {
+
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	// 發送通知
+	@PostMapping("/send")
+	public ResponseEntity<?> sendNotification(@RequestBody NotifyRequest request) {
+
+		Integer receiverId = request.getReceiveEmployeeId();
+
+		Integer approvalStepId = request.getApprovalStepId();
+
+		String message = request.getMessage();
+
+		Optional<NotifyResponse> result = notifyService.sendNotification(receiverId, approvalStepId, message);
+
+		if (result.isPresent()) {
+			return ResponseEntity.ok(result.get());
+		} else {
+			return ResponseEntity.badRequest().body("通知建立失敗");
+		}
+	}
 
 }
