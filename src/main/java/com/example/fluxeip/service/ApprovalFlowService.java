dba25f6af1b3b0020d84c09f0018a1563ff82083
Mqsun <shawn.sun0328@gmail.com>
2025-03-30 17:46:03 +0800

    假單和加減班的申請及簽核通知

--- src/main/java/com/example/fluxeip/service/ApprovalFlowService.java
+++ src/main/java/com/example/fluxeip/service/ApprovalFlowService.java
@@ -78,6 +78,9 @@ public class ApprovalFlowService {
 	
 	@Autowired
 	private ExpenseRequestRepository expenseRequestRepository;
+	
+	@Autowired
+	private NotifyService notifyService;
 
 	// 查詢員工待審核的請假單
 	@Transactional
@@ -169,7 +172,13 @@ public class ApprovalFlowService {
 
 		// 儲存簽核步驟
 		approvalStepRepository.save(approvalStep);
+		
+	    // 發送通知給第一關審核人
+		String typeName = leaveRequest.getLeaveType().getTypeName(); 
+		String employeeName = leaveRequest.getEmployee().getEmployeeName();
 
+		String message = "您有一筆新的請假申請待審核：員工 " + employeeName + " 的" + typeName + "單。";
+		notifyService.sendNotification(approver.getEmployeeId(), message);
 	}
 
 	// 請假簽核中
@@ -200,6 +209,11 @@ public class ApprovalFlowService {
 					.orElseThrow(() -> new RuntimeException("狀態不存在")));
 
 			leaveRequestRepository.save(leaveRequest);
+						
+			 // 通知申請人審核失敗
+	        String message = "您的請假申請《" + leaveRequest.getLeaveType().getTypeName() + "》未通過審核。";
+	        notifyService.sendNotification(leaveRequest.getEmployee().getEmployeeId(), message);		
+					
 			return "已否決請假單";
 		}
 
@@ -216,6 +230,11 @@ public class ApprovalFlowService {
 			leaveRequest.setStatus(statusRepository.findByStatusNameAndStatusType("已核決", "表單狀態")
 					.orElseThrow(() -> new RuntimeException("狀態不存在")));
 			leaveRequestRepository.save(leaveRequest);
+			
+			// 通知申請人審核通過
+	        String message = "您的請假申請《" + leaveRequest.getLeaveType().getTypeName() + "》已通過審核。";
+	        notifyService.sendNotification(leaveRequest.getEmployee().getEmployeeId(), message);
+
 			return "簽核成功";
 		}
 
@@ -258,6 +277,15 @@ public class ApprovalFlowService {
 			nextStep.setUpdatedAt(LocalDateTime.now());
 
 			approvalStepRepository.save(nextStep);
+			
+			// 通知下一位審核人
+			String typeName = leaveRequest.getLeaveType().getTypeName(); 
+			String employeeName = leaveRequest.getEmployee().getEmployeeName();
+
+			String message = "您有一筆新的請假申請待審核：員工 " + employeeName + " 的" + typeName + "單。";
+			notifyService.sendNotification(nextApprover.getEmployeeId(), message);
+			
+			
 		} else {
 			// 若沒有下一步，代表簽核完成，更新請假單狀態
 			Integer requestId = step.getRequestId();
@@ -266,6 +294,11 @@ public class ApprovalFlowService {
 			leaveRequest.setStatus(statusRepository.findByStatusNameAndStatusType("已核決", "表單狀態")
 					.orElseThrow(() -> new RuntimeException("狀態不存在")));
 			leaveRequestRepository.save(leaveRequest);
+			
+			// 通知申請人審核通過
+			String message = "您的請假申請《" + leaveRequest.getLeaveType().getTypeName() + "》已通過審核。";
+			notifyService.sendNotification(leaveRequest.getEmployee().getEmployeeId(), message);
+
 		}
 
 		return "簽核成功";
@@ -357,6 +390,14 @@ public class ApprovalFlowService {
 		// 儲存簽核步驟
 		approvalStepRepository.save(approvalStep);
 
+		// 發送通知：根據加班或減班顯示
+		String typeName = workAdjustmentRequest.getAdjustmentType().getTypeName(); // 例如 "加班" 或 "減班"
+		String employeeName = workAdjustmentRequest.getEmployee().getEmployeeName();
+
+		String message = "您有一筆新的"+ typeName +"申請待審核：員工" + employeeName + "的" + typeName +"單。";
+
+		notifyService.sendNotification(approver.getEmployeeId(), message);
+
 	}
 
 	// 加減班簽核中
@@ -387,6 +428,14 @@ public class ApprovalFlowService {
 					.orElseThrow(() -> new RuntimeException("狀態不存在")));
 
 			adjustmentRequestRepository.save(request);
+			
+
+	        // 通知申請人
+	        String typeName = request.getAdjustmentType().getTypeName(); // 加班 / 減班
+	        String msg = "您的《"+ typeName +"》申請未通過審核。";
+	        notifyService.sendNotification(request.getEmployee().getEmployeeId(), msg);
+			        
+			
 			return "已否決請假單";
 		}
 
@@ -403,6 +452,13 @@ public class ApprovalFlowService {
 			request.setStatus(statusRepository.findByStatusNameAndStatusType("已核決", "表單狀態")
 					.orElseThrow(() -> new RuntimeException("狀態不存在")));
 			adjustmentRequestRepository.save(request);
+			
+			//通知
+			String typeName = request.getAdjustmentType().getTypeName();
+	        String msg = "您的《"+ typeName +"》申請已通過審核。";
+	        notifyService.sendNotification(request.getEmployee().getEmployeeId(), msg);
+			
+	     
 			return "簽核成功";
 		}
 
@@ -443,6 +499,14 @@ public class ApprovalFlowService {
 			nextStep.setStatus(statusRepository.findByStatusNameAndStatusType("待審核", "表單狀態")
 					.orElseThrow(() -> new RuntimeException("狀態不存在")));
 			nextStep.setUpdatedAt(LocalDateTime.now());
+			
+			
+			//通知
+			String typeName = request.getAdjustmentType().getTypeName();
+		    String employeeName = request.getEmployee().getEmployeeName();
+		    String msg ="您有一筆新的"+ typeName +"申請待審核：員工" +employeeName+"的"+ typeName +"申請單。";
+		    notifyService.sendNotification(nextApprover.getEmployeeId(), msg);
+			
 
 			approvalStepRepository.save(nextStep);
 		} else {
@@ -453,12 +517,17 @@ public class ApprovalFlowService {
 			request.setStatus(statusRepository.findByStatusNameAndStatusType("已核決", "表單狀態")
 					.orElseThrow(() -> new RuntimeException("狀態不存在")));
 			adjustmentRequestRepository.save(request);
-		}
+			
+			// 通知申請人審核通過
+		    String message = "您的《" + request.getAdjustmentType().getTypeName() + "》申請已通過審核。";
+		    notifyService.sendNotification(request.getEmployee().getEmployeeId(), message);
 
+		}
+		
 		return "簽核成功";
 	}
 
-	// 查詢員工待審核的補卡單
+	// 查詢員工待審核的補卡單  《""》
 	@Transactional
 	public List<MissingPunchApprovalStepDTO> getPendingMissingPunchApprovalSteps(Integer approverId) {
 		// 查詢待審核的 ApprovalStep
@@ -696,7 +765,8 @@ public class ApprovalFlowService {
 			throw new RuntimeException("未找到對應的簽核流程");
 		}
 		ApprovalFlow firstStepFlow = firstStepFlowOpt.get();
-
+		
+		
 		// 找到該部門中符合該職位的第一位簽核人
 		Optional<Employee> approverOpt = employeeRepository
 				.findTopByPositionAndDepartmentAndStatus(firstStepFlow.getApproverPosition(),
@@ -714,7 +784,8 @@ public class ApprovalFlowService {
 
 		// 如果二次查詢仍然找不到簽核人，拋出異常
 		Employee approver = approverOpt.orElseThrow(() -> new RuntimeException("找不到該部門的簽核人，請確認設定"));
-
+		
+		
 		// 打印簽核人的姓名（可用於調試）
 		System.out.println(approver.getEmployeeName());
 
@@ -931,5 +1002,4 @@ public class ApprovalFlowService {
 			}
 		}
 	}
-
 }
